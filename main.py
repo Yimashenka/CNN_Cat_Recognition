a622be3c4981ea7ddd64ccbe47b6b7003e907f60
Yimashenka <angeloni.datascience@gmail.com>
2022-11-04 09:38:12 +0100

    Building the CNN

--- main.py
+++ main.py
@@ -37,3 +37,63 @@ test_set = test_datagen.flow_from_directory(
     batch_size=32,
     class_mode='binary'
 )
+
+## Step 3 : Building the CNN
+### Initialising the CNN
+cnn = tf.keras.models.Sequential()
+
+### 1 - Convolution :
+cnn.add(
+    tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=3,
+        activation='relu',
+        input_shape=[64, 64, 3]
+    )
+)
+
+### 2 - Pooling :
+cnn.add(
+    tf.keras.layers.MaxPool2D(
+        pool_size=(2, 2),
+        strides=2
+    )
+)
+
+### Adding a second Convolutional layer
+cnn.add(
+    tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=3,
+        activation='relu',
+        input_shape=[64, 64, 3]
+    )
+)
+
+cnn.add(
+    tf.keras.layers.MaxPool2D(
+        pool_size=(2, 2),
+        strides=2
+    )
+)
+
+### 3 - Flattening :
+cnn.add(
+    tf.keras.layers.Flatten()
+)
+
+### 4 - Full Connection :
+cnn.add(
+    tf.keras.Dense(
+        units=128,
+        activation='relu'
+    )
+)
+
+### 5 - Output layer :
+cnn.add(
+    tf.keras.Dense(
+        units=1,
+        activation='sigmoid'
+    )
+)
